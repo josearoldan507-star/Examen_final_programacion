c14afd43bf10dffb7506bf1f47f9201b0b24f930
josearoldan507-estrella <josearoldan507@gmail.com>
2026-04-14 02:28:52 -0500

    subiendo examen final

---
+++ MatchSimulator.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class MatchSimulator {
+
+    private Random random = new Random();
+
+    public void simularPartida(Player player1, Player player2) {
+
+        if (player1 == null || player2 == null) {
+            System.out.println("JUGADOR NO VALIDO");
+            return;
+        }
+
+        if (player1 == player2) {
+            System.out.println("NO SE PUEDE USAR EL MISMO JUGADOR");
+            return;
+        }
+
+        System.out.println("=================================================");
+        System.out.println("║              SIMULACIÓN DE PARTIDA            ║");
+        System.out.println("=================================================");
+
+        System.out.println(player1.getNick() + " VS " + player2.getNick());
+        System.out.println("-----------------------------");
+
+        int result = random.nextInt(3);
+
+        if (result == 0) {
+            System.out.println("-----------------------------");
+            System.out.println("GANA: " + player1.getNick());
+            player1.setPuntos(player1.getPuntos() + 10);
+
+        } else if (result == 1) {
+            System.out.println("-----------------------------");
+            System.out.println("GANA: " + player2.getNick());
+            player2.setPuntos(player2.getPuntos() + 10);
+            System.out.println("-----------------------------");
+
+        } else {
+            System.out.println("-----------------------------");
+            System.out.println("QUEDARON EMPATES");
+            player1.setPuntos(player1.getPuntos() + 5);
+            player2.setPuntos(player2.getPuntos() + 5);
+            System.out.println("-----------------------------");
+        }
+
+        System.out.println("\nPUNTOS ACTUALIZADOS:");
+        System.out.println("------------------------------");
+        System.out.println(player1.getNick() + ": " + player1.getPuntos());
+        System.out.println(player2.getNick() + ": " + player2.getPuntos());
+        System.out.println("==============================");
+    }
+}
