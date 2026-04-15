22c898645238ad71185808294e5b8e23e4569def
luisgarciaf2002-ctrl <luisgarciaf2002@gmail.com>
2026-04-14 19:56:01 -0500

    se agregaa a dev

--- Player.java
+++ Player.java
@@ -1,4 +1,23 @@
-
 public class Player {
 
+    private String nickname;
+    private int points;
+
+
+    public Player(String nickname, int points) {
+        this.nickname = nickname;
+        this.points = points;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
